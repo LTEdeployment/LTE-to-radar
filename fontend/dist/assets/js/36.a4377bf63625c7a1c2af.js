53bc4e6331b98b9ea1a6ac0e64ea3f6bb3db4975
XhinLiang <xhinliang@gmail.com>
2016-12-08 11:06:07 +0800

    add dist

---
+++ fontend/dist/assets/js/36.a4377bf63625c7a1c2af.js
@@ -0,0 +1,2 @@
+webpackJsonp([36,40],{262:function(e,t,n){"use strict";t.default={}},544:function(module,exports){module.exports={render:function(){with(this)return _h("div",[_h("router-view")])},staticRenderFns:[]}},84:function(e,t,n){var o,r;o=n(262);var i=n(544);r=o=o||{},"object"!=typeof o.default&&"function"!=typeof o.default||(r=o=o.default),"function"==typeof r&&(r=r.options),r.render=i.render,r.staticRenderFns=i.staticRenderFns,e.exports=o}});
+//# sourceMappingURL=36.a4377bf63625c7a1c2af.js.map
